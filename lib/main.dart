553c15977aa7357b449cc18f286f1c7e2d478a37
al-ayubi2020 <al.ayubi2020@gmail.com>
2022-11-14 10:42:37 +0700

    tambahBudgetPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:counter_7/tambahBudget.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -76,7 +77,52 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Counter Tugas 7 PBP"),
+          title: const Text("Counter Tugas 7 PBP"),
+        ),
+        drawer: Drawer(
+          child: Column(
+            children: [
+              // Menambahkan clickable menu
+              const Padding(
+                padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
+              ),
+              ListTile(
+                title: const Text('Counter'),
+                onTap: () {
+                  // Route menu ke halaman utama
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            const MyHomePage(title: 'Flutter Demo Home Page')),
+                  );
+                },
+              ),
+              // ignore: prefer_const_constructors
+              ListTile(
+                title: const Text('Tambah Budget'),
+                onTap: () {
+                  // Route menu ke halaman form
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const TambahBudgetPage()),
+                  );
+                },
+              ),
+              // ignore: prefer_const_constructors
+              ListTile(
+                title: const Text('Daftar Budget'),
+                // onTap: () {
+                //   // Route menu ke halaman form
+                //   Navigator.pushReplacement(
+                //     context,
+                //     MaterialPageRoute(builder: (context) => const MyFormPage()),
+                //   );
+                // },
+              ),
+            ],
+          ),
         ),
         body: Center(
           child: Column(
---
+++ lib/tambahBudget.dart
@@ -0,0 +1,211 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:counter_7/main.dart';
+import 'package:flutter/services.dart';
+
+class TambahBudgetPage extends StatefulWidget {
+  const TambahBudgetPage({super.key});
+
+  @override
+  State<TambahBudgetPage> createState() => _TambahBudgetPage();
+}
+
+class _TambahBudgetPage extends State<TambahBudgetPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  // ignore: unused_field
+  String _judul = "";
+
+  // ignore: unused_field
+  String _nominal = "";
+
+  String jenis = 'Pemasukan';
+
+  List<String> listJenis = ['Pemasukan', 'Pengeluaran'];
+
+  List<List<String>> data = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Form'),
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            // Menambahkan clickable menu
+            const Padding(
+              padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
+            ),
+            ListTile(
+              title: const Text('Counter'),
+              onTap: () {
+                // Route menu ke halaman utama
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          const MyHomePage(title: 'Flutter Demo Home Page')),
+                );
+              },
+            ),
+            // ignore: prefer_const_constructors
+            ListTile(
+              title: const Text('Tambah Budget'),
+              onTap: () {
+                // Route menu ke halaman form
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const TambahBudgetPage()),
+                );
+              },
+            ),
+            // ignore: prefer_const_constructors
+            ListTile(
+              title: const Text('Daftar Budget'),
+              // onTap: () {
+              //   // Route menu ke halaman form
+              //   Navigator.pushReplacement(
+              //     context,
+              //     MaterialPageRoute(builder: (context) => const MyFormPage()),
+              //   );
+              // },
+            ),
+          ],
+        ),
+      ),
+      body: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Container(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(children: [
+                Padding(
+                  // Menggunakan padding sebesar 8 pixels
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Contoh: Beli makan",
+                      labelText: "Judul",
+
+                      // Menambahkan circular border agar lebih rapi
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                    ),
+                    // Menambahkan behavior saat nama diketik
+                    onChanged: (String? value) {
+                      setState(() {
+                        _judul = value!;
+                      });
+                    },
+                    // Menambahkan behavior saat data disimpan
+                    onSaved: (String? value) {
+                      setState(() {
+                        _judul = value!;
+                      });
+                    },
+                    // Validator sebagai validasi form
+                    validator: (String? value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Judul tidak boleh kosong!';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  // Menggunakan padding sebesar 8 pixels
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Contoh: 100000",
+                      labelText: "Nominal",
+
+                      // Menambahkan circular border agar lebih rapi
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                    ),
+                    keyboardType: TextInputType.number,
+                    inputFormatters: <TextInputFormatter>[
+                      FilteringTextInputFormatter.digitsOnly
+                    ],
+                    // Menambahkan behavior saat nama diketik
+                    onChanged: (String? value) {
+                      setState(() {
+                        _nominal = value!;
+                      });
+                    },
+                    // Menambahkan behavior saat data disimpan
+                    onSaved: (String? value) {
+                      setState(() {
+                        _nominal = value!;
+                      });
+                    },
+                    // Validator sebagai validasi form
+                    validator: (String? value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Nominal tidak boleh kosong!';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                ListTile(
+                  title: const Text(
+                    'Pilih Jenis',
+                  ),
+                  trailing: DropdownButton(
+                    value: jenis,
+                    icon: const Icon(Icons.keyboard_arrow_down),
+                    items: listJenis.map((String items) {
+                      return DropdownMenuItem(
+                        value: items,
+                        child: Text(items),
+                      );
+                    }).toList(),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        jenis = newValue!;
+                      });
+                    },
+                  ),
+                ),
+                TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.blue),
+                  ),
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      // data.add([_judul, _nominal]);
+                      showDialog(
+                          context: context,
+                          builder: (BuildContext context) => AlertDialog(
+                                title: const Text('Info'),
+                                content: const Text('Berhasil tambah'),
+                                actions: <Widget>[
+                                  TextButton(
+                                    onPressed: () =>
+                                        Navigator.pop(context, 'OK'),
+                                    child: const Text('OK'),
+                                  ),
+                                ],
+                              ));
+                    }
+                    _formKey.currentState?.reset();
+                  },
+                  child: const Text(
+                    "Simpan",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ]),
+            ),
+          )),
+    );
+  }
+}
